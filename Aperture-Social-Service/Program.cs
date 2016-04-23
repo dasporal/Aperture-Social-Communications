8abfdde8a587dc8036f92e2c1543846e03a352f2
Dasporal <dasporal@gmail.com>
2016-04-23 22:18:35 +0200

    initial comit

---
+++ Aperture-Social-Service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aperture_Social_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Aperture Social Service
+
+##Presentation.
+
+A light console application for Twitter that I've been wanting to create for quite some time. Might not be suitable for long term use, but eh.

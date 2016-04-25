fb7a6ed65f4b23be6ed6565cfbec41a36441b710
Dasporal <dasporal@gmail.com>
2016-04-25 21:26:28 +0200

    updated REAMDE.md

--- Aperture-Social-Service/Authentication.cs
+++ Aperture-Social-Service/Authentication.cs
@@ -8,18 +8,18 @@ namespace Aperture_Social_Service
     class Authentication
     {
         ConsumerToken token = new ConsumerToken();
-        public void Authentify()
+        public ITwitterCredentials Authentify()
         {
-
             var appCredentials = new TwitterCredentials(token.consumerKey, token.consumerSecret);
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.Write("Please enter the PIN given by Twitter : ");
+            Console.Write("  Please enter the PIN given by Twitter : ");
             Console.ResetColor();
             var authenticationContext = AuthFlow.InitAuthentication(appCredentials);
             Process.Start(authenticationContext.AuthorizationURL);
             var pinCode = Console.ReadLine();
             var userCredentials = AuthFlow.CreateCredentialsFromVerifierCode(pinCode, authenticationContext);
             Auth.SetCredentials(userCredentials);
+            return userCredentials;
         }
         public void AdminAuthentify()
         {
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 ##Presentation.
 
-A light console application for Twitter that I've been wanting to create for quite some time. Might not be suitable for long term use, but eh.
+A light console application for Twitter that I've been wanting to create for quite some time. Might not be suitable for long term use, but eh. Let's be honest, this is mainly for muh personal branling and nerd-ish stuff.
+Tho I'm quite motivated into making a full UI material-ish client, but I'm still trying to figure out the shit ass limited API Twitter made for third party apps.
 
 ##Previews
 
-As of 04.24.2016, tweeting works and that's pretty much everything.
-![preview1](/previews/preview1.png)
-![tweetPreview1](/previews/previewTweet1.png)
+Everything shown in the "asc help" command works.
+![preview](http://i.imgur.com/S3oWy9m.gif)
+
+The tweet :
+
+![tweetPreview](http://i.imgur.com/oVzNx0M.png#center)
+
+A better view of the available commands :
+![commandsPreview](http://i.imgur.com/bhuk9Qp.png)
